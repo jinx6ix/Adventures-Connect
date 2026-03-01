33bb0aac4f828637bbe99f477756fd00dc9c887a
Peter Gitau <116557037+p3t3rkamau@users.noreply.github.com>
2026-03-01 17:10:54 +0300

    Remove large set of legacy files; add home styles
    Cleanup commit that removes a large number of legacy templates, workflows, pages, images, asset folders and backend files to trim repository bloat. Adds css/home-styles.css and updates index.html. This is intended to consolidate/remove outdated content and introduce a new home stylesheet; verify remaining required files and adjust any references to deleted assets.

--- backend/src/config/index.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    ServerConfig: require('./server-config'),
-    Logger: require('./logger-config'),
-    DatabaseConfig: require('./db-config'),
-}
--- backend/src/middlewares/index.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    UserMiddlewares:require('./user-middlewares')
-}
